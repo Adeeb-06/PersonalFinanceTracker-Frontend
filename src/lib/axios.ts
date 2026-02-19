df230ed13f90a4a6a4d1bf1aca26fc66f99cab3d
Adeeb-06 <mohammadadeeb886@gmail.com>
2026-02-19 21:46:25 +0600

    url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/",
+  baseURL: "https://trackitbackend-two.vercel.app/",
 });
 
 api.interceptors.request.use(async (config) => {
